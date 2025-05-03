6ddc305fe66679dff7ae37ec3d74abd1130e48f4
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-05-03 14:06:01 +0530

    fix bugs

--- Backend/src/routes/problem.routes.js
+++ Backend/src/routes/problem.routes.js
@@ -14,7 +14,7 @@ const router = Router();
 router.route("/create-problem").post(isLoggedIn, isAdmin, createProblem);
 router.route("/get-all-problems").get(isLoggedIn, getAllProblems);
 router.route("/get-problem/:id").get(isLoggedIn, getProblemById);
-router.route("/update-problem:/id").put(isLoggedIn, isAdmin, updateProblemById);
+router.route("/update-problem/:id").put(isLoggedIn, isAdmin, updateProblemById);
 router
     .route("/delete-problem/:id")
     .delete(isLoggedIn, isAdmin, deleteProblemById);
