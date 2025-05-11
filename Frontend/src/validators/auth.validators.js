1f7bcb6283c0bc82906b580a2e86759f05f850e9
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-05-12 03:37:38 +0530

    Add login

--- Frontend/src/validators/auth.validators.js
+++ Frontend/src/validators/auth.validators.js
@@ -6,4 +6,9 @@ const signUpSchema = z.object({
   password: z.string().min(6, "password must be atleast 6 character"),
 });
 
-export { signUpSchema }
+const loginSchema = z.object({
+  email: z.string().email("Enter a valid email"),
+  password: z.string().min(6, "Password must be atleast of 6 characters"),
+});
+
+export { signUpSchema, loginSchema }
