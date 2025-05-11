8f1f3431e939d2ed874c91502707a9eb58ece97a
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-05-12 02:24:45 +0530

    Initialize pinakAlgoX frontend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
+const App = () => {
+  return (
+    <div className="flex flex-col items-center justify-start">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
