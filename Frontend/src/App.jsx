bffc37d7253150ec94711e578b07b420935bad31
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-05-22 18:40:25 +0530

    Initialize the frontend of AlgoNex

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,30 +1,11 @@
-import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
+import React from 'react'
 
 const App = () => {
-  let authUser = null;
-
   return (
-    <div className="flex flex-col items-center justify-start">
-      <Routes>
-        <Route
-          path="/"
-          element={authUser ? <Home /> : <Navigate to={"/login"} />}
-        />
-        <Route
-          path="/login"
-          element={!authUser ? <Login /> : <Navigate to={"/"} />}
-        />
-        <Route
-          path="/signup"
-          element={!authUser ? <Signup /> : <Navigate to={"/"} />}
-        />
-      </Routes>
-    </div>
-  );
-};
+    <>
+      <h1>Hello AlgoNex</h1>
+    </>
+  )
+}
 
-export default App;
+export default App
