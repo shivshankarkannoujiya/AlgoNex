66933b0871bcb58ef3e6022df4ecd6418b783740
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-05-27 16:33:06 +0530

    Add custom testcase functionality

--- Frontend/src/pages/Problem.jsx
+++ Frontend/src/pages/Problem.jsx
@@ -36,6 +36,10 @@ const Problem = () => {
   const [selectedLanguage, setSelectedLanguage] = useState("javascript");
   const [testcases, setTestCases] = useState([]);
   const [isBookmarked, setIsBookmarked] = useState(false);
+  const [customTestCases, setCustomTestCases] = useState([
+    { input: "", output: "" },
+  ]);
+  const [activeTestIndex, setActiveTestIndex] = useState(0);
 
   useEffect(() => {
     dispatch(getProblemById(id));
@@ -63,13 +67,40 @@ const Problem = () => {
   };
 
   let submissionCount = 12;
+  const handleAddCustomTestCase = () => {
+    setCustomTestCases((prev) => [...prev, { input: "", output: "" }]);
+    setActiveTestIndex(customTestCases.length);
+  };
+
+  const handleDeleteCustomTestCase = (index) => {
+    const updated = [...customTestCases];
+    updated.splice(index, 1);
+    setCustomTestCases(updated.length ? updated : [{ input: "", output: "" }]);
+    setActiveTestIndex((prev) => (prev === index ? 0 : Math.max(0, prev - 1)));
+  };
+
+  const handleChangeCustomTestCase = (index, field, value) => {
+    const updated = [...customTestCases];
+    updated[index][field] = value;
+    setCustomTestCases(updated);
+  };
+
+  console.log("Submission: ", submission);
 
   const handleRunCode = async (e) => {
     e.preventDefault();
     try {
       const language_id = getJudge0LanguageId(selectedLanguage);
-      const stdin = problem.testcases?.map((tc) => tc?.input);
-      const expected_outputs = problem.testcases?.map((tc) => tc?.output);
+
+      const useCustom = customTestCases.some((tc) => tc.input.trim() !== "");
+
+      const stdin = useCustom
+        ? customTestCases.map((tc) => tc.input)
+        : problem.testcases?.map((tc) => tc.input);
+
+      const expected_outputs = useCustom
+        ? customTestCases.map((tc) => tc.output)
+        : problem.testcases?.map((tc) => tc.output);
 
       await dispatch(
         executeCode({
@@ -83,12 +114,10 @@ const Problem = () => {
 
       toast.success("Executed Successfully!");
     } catch (error) {
-      console.log("Error Executing Code: ", error);
-      toast.error("Error Executing Code");
+      console.error("Error Executing Code: ", error);
+      toast.error("Execution Failed");
     }
   };
-
-  console.log("Submission: ", submission);
 
   const renderTabContent = () => {
     switch (activeTab) {
@@ -198,7 +227,7 @@ const Problem = () => {
 
   return (
     <div className="min-h-screen  px-4 bg-[#000814] home-gradient text-white">
-      <nav className="flex items-center justify-between bg-[#000814] shadow-lg px-4 py-3 text-white">
+      <nav className="flex  items-center justify-between bg-[#000814] shadow-lg px-4 py-3 text-white">
         <div className="flex items-center gap-2">
           <Link
             to={"/dashboard"}
@@ -230,18 +259,18 @@ const Problem = () => {
         </div>
         <div className="flex items-center gap-4">
           <button
-            className={`p-2 rounded-full hover:bg-gray-500 text-white focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 ${
+            className={`p-2 rounded-full hover:bg-gray-500 text-white focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-opacity-50 ${
               isBookmarked ? "text-teal-600" : "text-gray-500"
             }`}
             onClick={() => setIsBookmarked(!isBookmarked)}
           >
             <Bookmark className="w-5 h-5" />
           </button>
-          <button className="p-2 rounded-full hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
+          <button className="p-2 rounded-full hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-opacity-50">
             <Share2 className="w-5 h-5" />
           </button>
           <select
-            className="block w-40 px-3 py-2 border border-gray-500 bg-gray-950 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+            className="block w-40 px-3 py-2 border border-gray-500 bg-gray-950 rounded-md shadow-sm focus:outline-none focus:ring-teal-500 focus:border-teal-500 sm:text-sm"
             value={selectedLanguage}
             onChange={handleLanguageChange}
           >
@@ -260,13 +289,12 @@ const Problem = () => {
             {" "}
             <div className="p-0">
               {" "}
-
               <div className="flex border-b border-gray-200 ">
                 {" "}
                 <button
-                  className={`flex-1 py-3 px-4 text-center text-gray-100 font-medium border-b-2 border-transparent hover:border-blue-500 hover:text-blue-500 transition-colors duration-200 ${
+                  className={`flex-1 py-3 px-4 text-center text-gray-100 font-medium border-b-2 border-transparent hover:border-teal-500 hover:text-teal-500 transition-colors duration-200 ${
                     activeTab === "description"
-                      ? "border-blue-500 text-blue-500"
+                      ? "border-teal-500 text-teal-500"
                       : ""
                   } flex items-center justify-center gap-2`}
                   onClick={() => setActiveTab("description")}
@@ -275,9 +303,9 @@ const Problem = () => {
                   Description
                 </button>
                 <button
-                  className={`flex-1 py-3 px-4 text-center text-gray-100 font-medium border-b-2 border-transparent hover:border-blue-500 hover:text-blue-500 transition-colors duration-200 ${
+                  className={`flex-1 py-3 px-4 text-center text-gray-100 font-medium border-b-2 border-transparent hover:border-teal-500 hover:text-teal-500 transition-colors duration-200 ${
                     activeTab === "submissions"
-                      ? "border-blue-500 text-blue-500"
+                      ? "border-teal-500 text-teal-500"
                       : ""
                   } flex items-center justify-center gap-2`}
                   onClick={() => setActiveTab("submissions")}
@@ -286,9 +314,9 @@ const Problem = () => {
                   Submissions
                 </button>
                 <button
-                  className={`flex-1 py-3 px-4 text-center text-gray-100 font-medium border-b-2 border-transparent hover:border-blue-500 hover:text-blue-500 transition-colors duration-200 ${
+                  className={`flex-1 py-3 px-4 text-center text-gray-100 font-medium border-b-2 border-transparent hover:border-teal-500 hover:text-teal-500 transition-colors duration-200 ${
                     activeTab === "discussion"
-                      ? "border-blue-500 text-blue-500"
+                      ? "border-teal-500 text-teal-500"
                       : ""
                   } flex items-center justify-center gap-2`}
                   onClick={() => setActiveTab("discussion")}
@@ -297,8 +325,8 @@ const Problem = () => {
                   Discussion
                 </button>
                 <button
-                  className={`flex-1 py-3 px-4 text-center text-gray-100 font-medium border-b-2 border-transparent hover:border-blue-500 hover:text-blue-500 transition-colors duration-200 ${
-                    activeTab === "hints" ? "border-blue-500 text-blue-500" : ""
+                  className={`flex-1 py-3 px-4 text-center text-gray-100 font-medium border-b-2 border-transparent hover:border-teal-500 hover:text-teal-500 transition-colors duration-200 ${
+                    activeTab === "hints" ? "border-teal-500 text-teal-500" : ""
                   } flex items-center justify-center gap-2`}
                   onClick={() => setActiveTab("hints")}
                 >
@@ -365,6 +393,83 @@ const Problem = () => {
                   }}
                 />
               </div>
+
+              {/* custom Testcases Start */}
+              <div className="p-4 border-t border-white">
+                <div className="flex items-center mb-4 space-x-2">
+                  {customTestCases.map((_, index) => (
+                    <button
+                      key={index}
+                      className={`px-3 py-1 rounded-t-md text-sm font-semibold ${
+                        activeTestIndex === index
+                          ? "bg-gray-800 text-teal-400"
+                          : "bg-gray-700 text-gray-300 hover:bg-gray-600"
+                      }`}
+                      onClick={() => setActiveTestIndex(index)}
+                    >
+                      Case {index + 1}
+                    </button>
+                  ))}
+                  <button
+                    onClick={handleAddCustomTestCase}
+                    className="px-3 py-1 rounded-t-md bg-gray-700 text-gray-300 hover:bg-gray-600 text-sm font-semibold"
+                  >
+                    +
+                  </button>
+                </div>
+
+                {customTestCases[activeTestIndex] && (
+                  <div className="bg-[#000814] p-4 rounded-b-md relative">
+                    {customTestCases.length > 1 && (
+                      <button
+                        onClick={() =>
+                          handleDeleteCustomTestCase(activeTestIndex)
+                        }
+                        className="absolute top-2 right-2 text-red-500 hover:text-red-400 text-sm"
+                        title="Delete this case"
+                      >
+                        ✕
+                      </button>
+                    )}
+
+                    <label className="block text-sm text-gray-400 mb-1">
+                      Input
+                    </label>
+                    <input
+                      type="text"
+                      className="w-full p-2 mb-3 rounded bg-black text-white border border-gray-600 focus:outline-none focus:ring-2 focus:ring-teal-500"
+                      value={customTestCases[activeTestIndex].input}
+                      onChange={(e) =>
+                        handleChangeCustomTestCase(
+                          activeTestIndex,
+                          "input",
+                          e.target.value,
+                        )
+                      }
+                      placeholder="...."
+                    />
+
+                    <label className="block text-sm text-gray-400 mb-1">
+                      Output
+                    </label>
+                    <input
+                      type="text"
+                      className="w-full p-2 rounded outline-none text-white border border-gray-600 focus:outline-none focus:ring-1 focus:ring-teal-500 "
+                      value={customTestCases[activeTestIndex].output}
+                      onChange={(e) =>
+                        handleChangeCustomTestCase(
+                          activeTestIndex,
+                          "output",
+                          e.target.value,
+                        )
+                      }
+                      placeholder="...."
+                    />
+                  </div>
+                )}
+              </div>
+              {/* Custom Testcses End */}
+
               <div className="p-4 border-t-2 border-white">
                 <div className="flex items-center justify-between">
                   <button
@@ -396,8 +501,8 @@ const Problem = () => {
                 <h3 className="text-xl font-bold text-teal-500">Test Cases</h3>
               </div>
               <div className="overflow-x-auto">
-                <table className="min-w-full divide-y divide-gray-200 border-collapse">
-                  <thead className="bg-[#0A1128] text-white">
+                <table className="min-w-full divide-y divide-gray-400 border-collapse">
+                  <thead className="bg-[#000814] text-white">
                     <tr>
                       <th className="px-6 py-3 text-left text-[16px] font-medium text-gray-300 uppercase tracking-wider">
                         Input
@@ -407,13 +512,13 @@ const Problem = () => {
                       </th>
                     </tr>
                   </thead>
-                  <tbody className="bg-[#0A1128] divide-y divide-gray-200 text-white">
+                  <tbody className="bg-[#000814] divide-y divide-gray-400 text-white">
                     {testcases?.length > 0 ? (
                       testcases.map((testCase, index) => (
                         <tr
                           key={index}
                           className={
-                            index % 2 === 1 ? "bg-gray-700" : "bg-gray-800"
+                            index % 2 === 1 ? "bg-gray-900" : "bg-gray-950"
                           }
                         >
                           <td className="px-6 py-4 whitespace-nowrap text-xl font-mono text-gray-100 font-semibold">
@@ -441,6 +546,7 @@ const Problem = () => {
           )}
         </div>
       </div>
+      
     </div>
   );
 };
