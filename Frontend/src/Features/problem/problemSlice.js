c0112a3aa482fe7691a9f3c8f0754e13113c75cb
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-05-28 03:26:54 +0530

    Add delete probleThunk

--- Frontend/src/Features/problem/problemSlice.js
+++ Frontend/src/Features/problem/problemSlice.js
@@ -1,76 +1,92 @@
 import { createSlice } from "@reduxjs/toolkit";
 import {
-    getAllProblems,
-    getProblemById,
-    getSolvedProblemByUser,
+  getAllProblems,
+  getProblemById,
+  getSolvedProblemByUser,
+  deleteProblem,
 } from "./problemThunks";
 
 const initialState = {
-    problems: [],
-    solvedProblems: [],
-    problem: null,
-    error: null,
-    isProblemsLoading: false,
-    isProblemLoading: false,
-    isSolvedProblemsLoading: false,
+  problems: [],
+  solvedProblems: [],
+  problem: null,
+  error: null,
+  isProblemsLoading: false,
+  isProblemLoading: false,
+  isDeletingProblem: false,
+  isSolvedProblemsLoading: false,
 };
 
 const problemSlice = createSlice({
-    name: "problems",
-    initialState,
-    reducers: {
-        resetProblems: (state) => {
-            state.problems = [];
-            state.solvedProblems = [];
-            state.problem = null;
-            state.error = null;
-            state.isProblemLoading = false;
-            state.isProblemsLoading = false;
-            state.isSolvedProblemsLoading = false;
-        },
+  name: "problems",
+  initialState,
+  reducers: {
+    resetProblems: (state) => {
+      state.problems = [];
+      state.solvedProblems = [];
+      state.problem = null;
+      state.error = null;
+      state.isProblemLoading = false;
+      state.isProblemsLoading = false;
+      state.isSolvedProblemsLoading = false;
     },
-    extraReducers: (builder) => {
-        builder
-            .addCase(getAllProblems.pending, (state) => {
-                state.error = null;
-                state.isProblemsLoading = true;
-            })
-            .addCase(getAllProblems.fulfilled, (state, action) => {
-                state.error = null;
-                state.isProblemsLoading = false;
-                state.problems = action.payload;
-            })
-            .addCase(getAllProblems.rejected, (state, action) => {
-                state.isProblemsLoading = false;
-                state.error = action.error.message;
-            })
-            .addCase(getProblemById.pending, (state) => {
-                state.error = null;
-                state.isProblemLoading = true;
-            })
-            .addCase(getProblemById.fulfilled, (state, action) => {
-                state.error = null;
-                state.isProblemLoading = false;
-                state.problem = action.payload;
-            })
-            .addCase(getProblemById.rejected, (state, action) => {
-                state.isProblemLoading = false;
-                state.error = action.error.message;
-            })
-            .addCase(getSolvedProblemByUser.pending, (state) => {
-                state.error = null;
-                state.isSolvedProblemsLoading = true;
-            })
-            .addCase(getSolvedProblemByUser.fulfilled, (state, action) => {
-                state.error = null;
-                state.isSolvedProblemsLoading = false;
-                state.solvedProblems = action.payload;
-            })
-            .addCase(getSolvedProblemByUser.rejected, (state, action) => {
-                state.isSolvedProblemsLoading = false;
-                state.error = action.error.message;
-            });
-    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getAllProblems.pending, (state) => {
+        state.error = null;
+        state.isProblemsLoading = true;
+      })
+      .addCase(getAllProblems.fulfilled, (state, action) => {
+        state.error = null;
+        state.isProblemsLoading = false;
+        state.problems = action.payload;
+      })
+      .addCase(getAllProblems.rejected, (state, action) => {
+        state.isProblemsLoading = false;
+        state.error = action.error.message;
+      })
+      .addCase(getProblemById.pending, (state) => {
+        state.error = null;
+        state.isProblemLoading = true;
+      })
+      .addCase(getProblemById.fulfilled, (state, action) => {
+        state.error = null;
+        state.isProblemLoading = false;
+        state.problem = action.payload;
+      })
+      .addCase(getProblemById.rejected, (state, action) => {
+        state.isProblemLoading = false;
+        state.error = action.error.message;
+      })
+      .addCase(getSolvedProblemByUser.pending, (state) => {
+        state.error = null;
+        state.isSolvedProblemsLoading = true;
+      })
+      .addCase(getSolvedProblemByUser.fulfilled, (state, action) => {
+        state.error = null;
+        state.isSolvedProblemsLoading = false;
+        state.solvedProblems = action.payload;
+      })
+      .addCase(getSolvedProblemByUser.rejected, (state, action) => {
+        state.isSolvedProblemsLoading = false;
+        state.error = action.error.message;
+      })
+      .addCase(deleteProblem.pending, (state) => {
+        state.error = null;
+        state.isDeletingProblem = true;
+      })
+      .addCase(deleteProblem.fulfilled, (state) => {
+        state.error = null;
+        state.isDeletingProblem = false;
+        state.problem = null;
+        state.problems = state.problems.filter((p) => p.id !== action.meta.arg);
+      })
+      .addCase(deleteProblem.rejected, (state, action) => {
+        state.isDeletingProblem = false;
+        state.error = action.payload;
+      });
+  },
 });
 
 export const { resetProblems } = problemSlice.actions;
--- Frontend/src/Features/problem/problemThunks.js
+++ Frontend/src/Features/problem/problemThunks.js
@@ -2,48 +2,64 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import apiClient from "../../Service/apiClient";
 
 const getAllProblems = createAsyncThunk(
-    "problem/getAll",
-    async (_, thunkAPI) => {
-        try {
-            const res = await apiClient.getAllProblems();
-            console.log(res);
-            console.log(res.data);
-            console.log(res.data.problems);
-            return res.data?.problems;
-        } catch (error) {
-            return thunkAPI.rejectWithValue(error.message);
-        }
-    },
+  "problem/getAll",
+  async (_, thunkAPI) => {
+    try {
+      const res = await apiClient.getAllProblems();
+      console.log(res);
+      console.log(res.data);
+      console.log(res.data.problems);
+      return res.data?.problems;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  },
 );
 
 const getProblemById = createAsyncThunk(
-    "problme/getById",
-    async (id, thunkAPI) => {
-        try {
-            const res = await apiClient.getProblemById(id);
-            console.log(res);
-            console.log(res.data);
-            console.log(res.data.problem);
-            return res.data?.problem;
-        } catch (error) {
-            return thunkAPI.rejectWithValue(error.message);
-        }
-    },
+  "problme/getById",
+  async (id, thunkAPI) => {
+    try {
+      const res = await apiClient.getProblemById(id);
+      console.log(res);
+      console.log(res.data);
+      console.log(res.data.problem);
+      return res.data?.problem;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  },
 );
 
 const getSolvedProblemByUser = createAsyncThunk(
-    "/problem/solved",
-    async (_, thunkAPI) => {
-        try {
-            const res = await apiClient.getSolvedProblem();
-            console.log(res);
-            console.log(res.data);
-            console.log(res.data.problems);
-            return res.data?.problems;
-        } catch (error) {
-            return thunkAPI.rejectWithValue(error.message);
-        }
-    },
+  "/problem/solved",
+  async (_, thunkAPI) => {
+    try {
+      const res = await apiClient.getSolvedProblem();
+      console.log(res);
+      console.log(res.data);
+      console.log(res.data.problems);
+      return res.data?.problems;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  },
 );
 
-export { getAllProblems, getProblemById, getSolvedProblemByUser };
+const deleteProblem = createAsyncThunk(
+  "/problem/delete",
+  async (id, thunkAPI) => {
+    try {
+      await apiClient.deleteProblem(id);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  },
+);
+
+export {
+  getAllProblems,
+  getProblemById,
+  getSolvedProblemByUser,
+  deleteProblem,
+};
