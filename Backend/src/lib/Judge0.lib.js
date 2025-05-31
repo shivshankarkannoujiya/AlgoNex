8cbd27cfed7a51671672f49dfc2272df04463d3d
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-06-01 00:47:34 +0530

    Remove unused codes

--- Backend/src/lib/Judge0.lib.js
+++ Backend/src/lib/Judge0.lib.js
@@ -25,21 +25,6 @@ export const getLanguageNameById = (language_id) => {
     return LANGUAGE_NAMES[language_id] || "Unknown";
 };
 
-/**
- * @description
- * This method will hit the judge0 end point
- */
-// export const submitBatch = async (submission) => {
-//     const { data } = await axios.post(
-//         `${process.env.JUDGE0_API_URL}/submissions/batch?base64_encoded=false`,
-//         {
-//             submission,
-//         },
-//     );
-
-//     console.log(`Submission Result: `, data);
-//     return data; // [{token}, {token}, {token}]
-// };
 
 export const submitBatch = async (submissions) => {
     try {
@@ -68,30 +53,6 @@ export const submitBatch = async (submissions) => {
 
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-// export const pollBatchResults = async (tokens) => {
-//     while (true) {
-//         const { data } = await axios.get(
-//             `${process.env.JUDGE0_API_URL}/submissions/batch`,
-//             {
-//                 params: {
-//                     tokens: tokens.join(","),
-//                     base64_encoded: false,
-//                 },
-//             },
-//         );
-
-//         const results = data.submissions;
-//         const isAllDone = results.every(
-//             (result) => result.status.id !== 1 && result.status.id !== 2,
-//         );
-
-//         if (!isAllDone) {
-//             await sleep(1000);
-//         }
-
-//         return results;
-//     }
-// };
 
 export const pollBatchResults = async (tokens) => {
     try {
