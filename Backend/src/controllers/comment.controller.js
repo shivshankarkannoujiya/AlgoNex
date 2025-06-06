2169947f34ee936725453fb7f4755df8ef193d48
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-06-07 03:00:26 +0530

    remove logs

--- Backend/src/controllers/comment.controller.js
+++ Backend/src/controllers/comment.controller.js
@@ -1,7 +1,6 @@
 import { prisma } from "../lib/db.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiResponse } from "../utils/api-response.js";
-import { ApiError } from "../utils/api-error.js";
 import { UserRole } from "../generated/prisma/index.js";
 
 const createComment = asyncHandler(async (req, res) => {
--- Backend/src/utils/cloudinary.js
+++ Backend/src/utils/cloudinary.js
@@ -12,11 +12,6 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-console.table([
-    process.env.CLOUDINARY_CLOUD_NAME,
-    process.env.CLOUDINARY_API_KEY,
-    process.env.CLOUDINARY_API_SECRET,
-]);
 
 const uploadOnCloudinary = async (localFilePath) => {
     try {
