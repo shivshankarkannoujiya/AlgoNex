3db6816c2fcc91be7424ce8c45e43afdbe05c37e
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-06-07 15:26:56 +0530

    fix bugs

--- Frontend/src/Store/store.js
+++ Frontend/src/Store/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "../features/auth/authSlice";
+import authReducer from "../features/auth/authSlice"
 import problemReducer from "../features/problem/problemSlice";
 import executionReducer from "../features/execution/executionSlice";
 import submissionReducer from "../features/submission/submissionSlice";
