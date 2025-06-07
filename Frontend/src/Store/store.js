08ff23ec7a7e32623d1ddba7cebf14f7f44c294b
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-06-07 15:31:34 +0530

    fix

--- Frontend/src/Store/store.js
+++ Frontend/src/Store/store.js
@@ -1,9 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "../features/auth/authSlice"
-import problemReducer from "../features/problem/problemSlice";
-import executionReducer from "../features/execution/executionSlice";
-import submissionReducer from "../features/submission/submissionSlice";
-import playlistReducer from "../features/playlist/playlistSlice";
+import authReducer from "../features/auth/authSlice.js"
+import problemReducer from "../features/problem/problemSlice.js";
+import executionReducer from "../features/execution/executionSlice.js";
+import submissionReducer from "../features/submission/submissionSlice.js";
+import playlistReducer from "../features/playlist/playlistSlice.js";
 
 const store = configureStore({
   reducer: {
